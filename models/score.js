48b87d3472983a08483d9f0ab5e2264cce9ac339
Mic Sha <shattuck.michael@gmail.com>
2020-08-29 13:45:05 -0400

    added model score.js

--- models/score.js
+++ models/score.js
@@ -1,3 +1,39 @@
-module.exports = function(sequelize, DataTypes) {
+/* eslint-disable camelcase */
+/* eslint-disable prettier/prettier */
+/* eslint-disable indent */
+module.exports = function(sequelize, DataTypes) 
+{
+	const Score = sequelilze.define("score",
+	{
+		name:
+		{
+			type: DataTypes.SRING,
+			allowNull: false,
+			validate:
+			{
+				len:[1,30]
+			}
+		},
 
+		score:
+			{
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			}			  
+	});
+	
+	Score.associate = function(models)
+	{
+		Score.belongsTo(models.Category, 
+		{
+			foreignKey: 
+			{
+				allowNull: false				
+			}
+    	});
+	  };
+
+   Score_table.sync();
+
+  return POST;
 };
