5a6b8f9416decd1d46c9262d1c5f762699d3dce8
Mic Sha <shattuck.michael@gmail.com>
2020-08-29 11:27:40 -0400

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -7,14 +7,21 @@ USE openquiz_db;
 CREATE TABLE player_table (
   id INT NOT NULL AUTO_INCREMENT,
   name varchar(30) NOT NULL,
-  score INT NOT NULL,
-  category INT NOT NULL,
+  email varchar(30),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE category_table(
+CREATE TABLE category_table
+(
     id INT NOT NULL,
     category_name varchar(30) NOT NULL,
     primary key (id)
 );
 
+CREATE TABLE score_table
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    score INT NOT NULL,
+    category_id INT NOT NULL, 
+    primary key(id)
+);
