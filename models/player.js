7f2a51e7839cb648f5da812a845c2b6108958276
Mic Sha <shattuck.michael@gmail.com>
2020-09-01 21:05:47 -0400

    updated player.js

--- models/player.js
+++ models/player.js
@@ -1,3 +1,16 @@
-module.exports = function(sequelize, DataTypes) {
+/* eslint-disable no-unused-vars */
+/* eslint-disable indent */
+/* eslint-disable prettier/prettier */
+const Sequelize = require("sequelize");
 
-};
+
+module.exports = function(sequelize, DataTypes) {
+    const Player = sequelize.define("Player", {
+      
+    name  : DataTypes.STRING,
+    email : DataTypes.STRING,
+      
+    });
+  
+    return Player;
+  };
