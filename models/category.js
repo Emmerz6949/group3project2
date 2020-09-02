3573902981f96e28463414fd42659f964ab23076
emmerz6949 <emmerz6949@gmail.com>
2020-09-01 20:44:50 -0400

    updated category.js

--- models/category.js
+++ models/category.js
@@ -1,3 +1,24 @@
 module.exports = function(sequelize, DataTypes) {
+    const Category = sequelize.define("category",{
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 30]
+            }
+        },
+        api_category_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isInt: true
+            }
+        }
+    });
 
+    Category.associate = function(models) {
+        Category.hasMany(models.Score);
+    };
+
+    return Category;
 };
