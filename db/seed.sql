f6a44256285c8dfc86eb14e0db627e193a51d0ba
Mic Sha <shattuck.michael@gmail.com>
2020-09-05 15:36:41 -0400

    updated server.js

--- db/seed.sql
+++ db/seed.sql
@@ -4,10 +4,8 @@ INSERT INTO category_table (category_name, api_category_id)
 VALUES ("Books",10),
        ("Film",11),
        ("Music",12),
-       ("Musicals and Theater",13),
        ("Television",14),
        ("Video Games",15),
-       ("Board Games",16),
        ("Comics",29),
        ("Anime and Manga",31),
        ("Cartoons",32);
